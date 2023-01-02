39f2ea57a58f0a7959ecd656ae977ea7ef1384d8
Ermachenkov Maxim <epmak.8383@mail.ru>
2023-01-02 20:38:40 +0300

    first

---
+++ Task_1/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1.
+
+int FirstRecurtion(int n)
+{
+    if (n == 1) return n;
+    n--;
+    Console.Write($"{n},  ");
+    FirstRecurtion(n);
+    return n;
+}
+
+int Enterdata(string text)
+{
+    Console.Write(text);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+int n = Enterdata("Введите число N:  ");
+Console.Write(n + ",  ");
+FirstRecurtion(n);
