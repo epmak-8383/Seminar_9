92a641c7d591ef7cda6ad9534fab3e01d3a141a3
Ermachenkov Maxim <epmak.8383@mail.ru>
2023-01-02 21:05:19 +0300

    second

---
+++ Task_2/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задайте значения M и N. Напишите программу,
+// которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+int Sum(int m, int n)
+{
+    if (n == m) return n;
+    if (m == n) return m;
+    return (m + Sum(m+1, n));
+}
+
+int Enterdata(string text)
+{
+    Console.Write(text);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+int m = Enterdata("Введите число M:  ");
+int n = Enterdata("Введите число N:  ");
+Sum(m, n);
+Console.Write(Sum(m, n));
